92a452a60f671cbbeb73b13f09ad91bce22580b2
andela-mabdussalam <mariam.abdussalam@andela.com>
2018-07-23 10:26:50 +0100

    add sequelize sync

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+import db from './models';
 import app from './app';
 
 const port = process.env.PORT || 3000;
+
+db.sequelize.sync({ force: true });
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
