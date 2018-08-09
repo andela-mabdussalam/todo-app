81ba807a9b4868d69ae980521cc813ef064ad57c
andela-mabdussalam <mariam.abdussalam@andela.com>
2018-08-09 11:06:17 +0100

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-import { UserController, Todos } from '../controllers';
+import { UserController, TodoController } from '../controllers';
 import { authenticate, inputValidator, verifyOwner } from '../middlewares';
 
 export default (app) => {
   app.post('/users', inputValidator, UserController.createUser);
   app.post('/users/login', UserController.login);
   app.put('/users/:id', authenticate, verifyOwner, UserController.updateUser);
-  // app.post('/todos', authenticate, Todos.create);
-  // app.get('/todos/:id', authenticate, Todos.getUserTodos);
-  // app.delete('/todos/:id', authenticate, Todos.deleteUserTodo);
-  // app.put('/todos/:id', authenticate, Todos.updateTodo);
+  app.post('/todos', authenticate, TodoController.create);
+  app.get('/todos/:id', authenticate, TodoController.getUserTodos);
+  app.delete('/todos/:id', authenticate, TodoController.deleteUserTodo);
+  app.put('/todos/:id', authenticate, TodoController.updateTodo);
 };
